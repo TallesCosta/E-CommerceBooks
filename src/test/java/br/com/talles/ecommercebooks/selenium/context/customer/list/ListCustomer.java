b116d9bff8911534c7bd9f07673f4636fe906985
Talles Costa <talles.sp.costa@outlook.com>
2017-10-08 02:55:06 -0300

    feature: teste de listagem de clientes

---
+++ src/test/java/br/com/talles/ecommercebooks/selenium/context/customer/list/ListCustomer.java
@@ -0,0 +1,24 @@
+package br.com.talles.ecommercebooks.selenium.context.customer.list;
+
+import br.com.talles.ecommercebooks.selenium.context.customer.CustomerT;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class ListCustomer extends CustomerT {
+	
+	@Test
+	public void saveCustomer(){
+		// Go to Home page
+		browser.get(index);
+		
+		// Go to List-Custumers page
+		WebElement element = browser.findElement(By.className(elements.LINK_LIST));
+		element.click();
+		
+		// Redirect list page? YES -> Passed the test!
+		Assert.assertNotEquals(null, browser.findElement(By.id(elements.PAGE_LIST)));
+	}
+	
+}
